0da252c77206f651d58dfa826718130e2cd3dab2
Candela Vulcano <candelavul@gmail.com>
2026-06-15 18:31:16 -0300

    Update __init__.py

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,11 @@
+from app.repositories.log_actividad import LogActividadRepository
+from app.repositories.propiedad import PropiedadRepository
+from app.repositories.reserva import ReservaRepository
+from app.repositories.usuario import UsuarioRepository
+
+__all__ = [
+    "LogActividadRepository",
+    "PropiedadRepository",
+    "ReservaRepository",
+    "UsuarioRepository",
+]
